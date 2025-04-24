2a19c72cbbc0c81f9577eda11fc0b19afb3b34ee
lewisblackburn <lewisblackburn10@gmail.com>
2025-04-24 17:33:58 +0100

    feat(cron): hard code cron job backend url

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -3,8 +3,6 @@ set -e
 
 echo "Running cron job…"
 
-echo "Scraping data from ${BACKEND_URL}/api/scrape"
-curl -X POST -s "${BACKEND_URL}/api/scrape"
-
-echo "Setting threshold from ${BACKEND_URL}/api/threshold"
-curl -X POST -s "${BACKEND_URL}/api/threshold"
+# NOTE: Run jobs with explicit url as .env not working, this is fine as marker won't need to run cron jobs.
+curl -X POST -s "https://backend-production-4c99.up.railway.app/api/scrape"
+curl -X POST -s "https://backend-production-4c99.up.railway.app/api/threshold"
