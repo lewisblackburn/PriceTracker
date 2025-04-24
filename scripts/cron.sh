8b616645d0bd03381304a59f1aa43371d12025ea
lewisblackburn <lewisblackburn10@gmail.com>
2025-04-24 17:29:26 +0100

    feat(cron): removed finding .env file as vars are gotten from environment

--- scripts/cron.sh
+++ scripts/cron.sh
@@ -1,16 +1,10 @@
 #!/usr/bin/env bash
 set -e
-set -o allexport
 
-source "$(dirname "$0")/.env"
-
-set +o allexport
-
-echo "Running cron job..."
+echo "Running cron job…"
 
 echo "Scraping data from ${BACKEND_URL}/api/scrape"
 curl -X POST -s "${BACKEND_URL}/api/scrape"
 
 echo "Setting threshold from ${BACKEND_URL}/api/threshold"
 curl -X POST -s "${BACKEND_URL}/api/threshold"
-
